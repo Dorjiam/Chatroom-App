2b0678f4c6b8d088b8a77b2ba88beb33dfe49720
Dorjiam <DorjiamNotKnown@gmail.com>
2024-04-25 18:48:09 +0330

    Basic GroupNavbar UI completed (Not that responsive)

--- apps/backend/chatroom_app/main/views.py
+++ apps/backend/chatroom_app/main/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "base.html")
+    user_info = {
+        "avatar_name": "avatar-1.png",
+        "username": "Dorjiam",
+        "bio": "Senior Developer",
+    }
+    return render(request, "components/group_navbar.html", user_info)
