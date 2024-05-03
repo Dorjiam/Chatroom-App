b8dd0b46743c817764998767aa4d132dcad67401
yasin <yasindavody1388@gmail.com>
2024-05-03 23:14:25 +0330

    change main-urls

--- apps/backend/chatroom_app/chatroom_app/urls.py
+++ apps/backend/chatroom_app/chatroom_app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from main.urls import urlpatterns as main_urls
 from django.urls import path, include
 from Admin.urls import urlpatterns as admin_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("main.urls")),
+    path("", include(main_urls)),
     path("Admin/", include(admin_urls))
 ]
